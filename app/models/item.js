4351fda8d7ba623f2cbde27fb3a539e158bae3fa
Amruta <arkamat@indiana.edu>
2016-11-17 12:31:45 -0500

    -- Added category and ad tables

--- app/models/item.js
+++ app/models/item.js
@@ -1,21 +1,47 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 var itemSchema = mongoose.Schema({
-    local         : {
-	name	 : String,
-	price	 : String,
-    category     : String,
-    email        : String,
-    phone		 : String,
-    password     : String,
-    }
+	item : {
+		title : String,
+		price : String,
+		category : String,
+		description : String,
+		username : [ {
+			type : String,
+			ref : 'User'
+		} ],
+		catId : [ {
+			type : Number,
+			ref : 'Category'
+		} ]
+	}
 });
 
+itemSchema.methods.updateitem = function(request, response) {
+	this.item.title = request.body.title;
+	this.item.price = request.body.price;
+	this.item.category = request.body.category;
+	this.item.username = request.user.local.email;
+	this.item.description = request.body.description;
 
-itemSchema.methods.update = function(request, response){
-	this.user.name = request.body.name;
-	this.user.save();
-	response.redirect('./app/models/user');
+	this.item.save(function(err) {
+		if (err) {
+			throw err;
+		}
+
+	});
 };
 
+/* deletes the product passed in as arguement from the products collection */
+itemSchema.methods.deleteitem = function(request, response) {
+	item.remove(function(err) {
+		if (err) {
+			throw err;
+		}
+
+	});
+}
+
 module.exports = mongoose.model('Item', itemSchema);
+// Item.find().populate("username");
